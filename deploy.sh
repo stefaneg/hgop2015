682e34515cfa7da6fd5b113d4315813830a1e52b
Katur <grimurk@gmail.com>
2015-11-30 12:32:41 +0000

    Pass server IP as parameter to deploy script, and small change to index.html to see if it works

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-docker push katur/tictactoe
-ssh root@95.85.44.69 'cd hgop2015 && git pull origin master && ./run.sh' 
+SERVER_IP=$1
+if [ -z $SERVER_IP ] ; then
+    SERVER_IP="95.85.44.69"
+fi
+
+sudo docker push katur/tictactoe
+ssh root@$SERVER_IP 'git clone git@github.com:Katur7/hgop2015.git && cd hgop2015 && ./run.sh' 
