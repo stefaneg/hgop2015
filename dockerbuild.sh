1709e1b5ca273274d4673b2f9165ad552b1c6fe1
Guðlaugur S. Egilsson <gulli@kolibri.is>
2015-12-11 15:29:19 +0000

    Use correct Github url. Remove source dir mapping from server Vagrantfile which was not supposed to be there.

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -4,6 +4,14 @@ echo Cleaning...
 rm -rf ./dist
 
 
+if [ -z "$GIT_COMMIT" ]; then
+  export GIT_COMMIT=$(git rev-parse HEAD)
+  export GIT_URL=$(git config --get remote.origin.url)
+fi
+
+# Remove .git from url in order to get https link to repo (assumes https url for GitHub)
+export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
+
 echo Building app
 grunt
 
@@ -13,11 +21,6 @@ if [[ $rc != 0 ]] ; then
     exit $rc
 fi
 
-
-if [ -z "$GIT_COMMIT" ]; then
-  export GIT_COMMIT=$(git rev-parse HEAD)
-  export GIT_URL=$(git config --get remote.origin.url)
-fi
 
 cat > ./dist/githash.txt <<_EOF_
 $GIT_COMMIT
@@ -29,10 +32,10 @@ cat > ./dist/public/version.html << _EOF_
    <title>TicTacToe version information</title>
 </head>
 <body>
-   <span>Origin:</span> <span>$GIT_URL</span>
+   <span>Origin:</span> <span>$GITHUB_URL</span>
    <span>Revision:</span> <span>$GIT_COMMIT</span>
    <p>
-   <div><a href="$GIT_URL/commits/$GIT_COMMIT">History of current version</a></div>
+   <div><a href="$GITHUB_URL/commits/$GIT_COMMIT">History of current version</a></div>
 </body>
 _EOF_
 
--- provisioning/production/docker-pull-latest-version.sh
+++ provisioning/production/docker-pull-latest-version.sh
@@ -3,4 +3,4 @@ echo Running revision $2 on port $1
 docker kill tictactoe
 docker rm tictactoe
 docker pull gulli/tictactoe:$2
-docker run -p 8080:$1 -d --name tictactoe -e "NODE_ENV=production" gulli/tictactoe:$2
+docker run -p $1:8080 -d --name tictactoe$1 -e "NODE_ENV=production" gulli/tictactoe:$2
