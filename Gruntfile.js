06f77f17e4a130f775764f1e7d81eb54fb41ad87
Guðlaugur S. Egilsson <gulli@kolibri.is>
2015-12-11 16:43:25 +0000

    Add support to specify test report name as an env variable.

--- Gruntfile.js
+++ Gruntfile.js
@@ -443,21 +443,21 @@ module.exports = function (grunt) {
       test: {
         options: {
           reporter: process.env.MOCHA_REPORTER || 'spec',
-          captureFile: 'server-tests'
+          captureFile: process.env.MOCHA_REPORT || 'server-tests.txt'
         },
         src: ['server/**/*.spec.js']
       },
       acceptance: {
         options: {
           reporter: process.env.MOCHA_REPORTER || 'spec',
-          captureFile: 'acceptance-tests'
+          captureFile: process.env.MOCHA_REPORT || 'acceptance-tests.txt'
         },
         src: ['server/**/*.acceptance.js']
       },
       load: {
         options: {
           reporter: process.env.MOCHA_REPORTER || 'spec',
-          captureFile: 'load-tests'
+          captureFile: process.env.MOCHA_REPORT || 'load-tests.txt'
         },
         src: ['server/**/*.load.js']
       }
