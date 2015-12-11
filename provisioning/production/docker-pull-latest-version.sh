1046ec45fe04aad6d6683043965b05f827b6e800
Guðlaugur S. Egilsson <gulli@kolibri.is>
2015-12-11 15:40:11 +0000

    Forgot to append port to container name to distinguish running containers.

--- provisioning/production/docker-pull-latest-version.sh
+++ provisioning/production/docker-pull-latest-version.sh
@@ -1,6 +1,6 @@
 echo Running revision $2 on port $1
 
-docker kill tictactoe
-docker rm tictactoe
+docker kill tictactoe$1
+docker rm tictactoe$1
 docker pull gulli/tictactoe:$2
 docker run -p $1:8080 -d --name tictactoe$1 -e "NODE_ENV=production" gulli/tictactoe:$2
